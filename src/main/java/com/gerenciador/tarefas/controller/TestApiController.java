bf1d94c6efb4bffa0126426df0d6476fd0356841
Lucas Fernando <lucashalves26@gmail.com>
2023-11-09 13:27:40 -0300

    Criado test Controller

---
+++ src/main/java/com/gerenciador/tarefas/controller/TestApiController.java
@@ -0,0 +1,13 @@
+package com.gerenciador.tarefas.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestApiController {
+
+    @GetMapping("/test-api")
+    private String test(){
+        return "Sucesso";
+    }
+}
